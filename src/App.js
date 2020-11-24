0dd7793849c013712e068147862db73220bc02fb
Dan Lenington <danlening02@gmail.com>
2020-11-24 15:52:12 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import axios from "axios";
+
 
 function App() {
+
+useEffect(() => {
+  getCurrentWeather();
+
+}, [])
+
+
+const getCurrentWeather = async () => {
+  try {
+    const config = {
+      params: {
+        key: '0b16e2bb3b094e029bb210414202411',
+        q: "Minneapolis"
+      }
+    }
+    const response = await axios.get("http://api.weatherapi.com/v1/current.json", config);
+    console.log("response", response);
+
+  } catch (error) {
+    console.log("Error", error)
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     Weather App
+   </div>
   );
 }
 
